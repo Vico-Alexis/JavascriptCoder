125cd97bc8c85eb66cf3a9c7a51fdbb1e3ad1e50
Alexis <alexisvico_7@hotmail.com>
2025-04-14 22:01:00 -0300

    convertidor dolar

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("main");
+function convertir() {
+    const tasaCambio = 0.00084; // 1 peso = "x" dolar
+    const pesos = parseFloat(document.getElementById("pesos").value);
+    const dolares = pesos * tasaCambio;
+    const mensaje = `${pesos} pesos son $${dolares} dólares.`;
+    document.getElementById('resultado').textContent = mensaje;
+    console.log(mensaje)
+}
